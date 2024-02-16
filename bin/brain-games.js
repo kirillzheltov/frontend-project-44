f23aed482a0c6e7d74e1ccdde9329db3921c17a2
Kirill Zheltov <kirillzheltov@gmail.com>
2024-02-15 22:33:54 -0300

    brain-games step 3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
+import { getName } from '../src/cli.js';
+
 const text = 'Welcome to the Brain Games!';
 console.log(text);
+
+
+const name = getName();
+console.log(`Hello, ${name}!`);
