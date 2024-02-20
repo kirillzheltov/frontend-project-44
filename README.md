447b98d6fcaf248b9492ec1c1ffbdcf96d31cf17
Kirill Zheltov <kirillzheltov@gmail.com>
2024-02-20 18:52:46 -0300

    brain-games step 7

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/eb45cd2236d68f83dfbc/maintainability)](https://codeclimate.com/github/kirillzheltov/frontend-project-44/maintainability)
 [![asciicast](https://asciinema.org/a/1GEpPwi1vWawq5rt9tActynLv.svg)](https://asciinema.org/a/1GEpPwi1vWawq5rt9tActynLv)
 [![asciicast](https://asciinema.org/a/raX2udxKs6psE0XlLkcTLhXj5.svg)](https://asciinema.org/a/raX2udxKs6psE0XlLkcTLhXj5)
+[![asciicast](https://asciinema.org/a/LFZjQLufCT9J7nadiW3waIXi2.svg)](https://asciinema.org/a/LFZjQLufCT9J7nadiW3waIXi2)
---
+++ src/games/brain-gcd.js
@@ -0,0 +1,40 @@
+import { getRandomNumber } from '../index.js';
+
+const gameDescription = 'Find the greatest common divisor of given numbers.';
+
+function getData() {
+  return {
+    number1: getRandomNumber(),
+    number2: getRandomNumber(),
+  };
+}
+
+function getQuestion(data) {
+  const { number1, number2 } = data;
+  const expression = `${number1} ${number2}`;
+  return expression;
+}
+
+function gcd(number1, number2) {
+  if (number1 === 0) {
+    return number2;
+  }
+  return gcd(number2 % number1, number1);
+}
+
+function getTrueAnswer(data) {
+  const { number1, number2 } = data;
+  const result = gcd(number1, number2);
+  return result.toString();
+}
+
+function getGameData() {
+  const data = getData();
+  return {
+    description: gameDescription,
+    question: getQuestion(data),
+    trueAnswer: getTrueAnswer(data),
+  };
+}
+
+export { getGameData };
